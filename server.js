af750118fbf0cd2e42d41bd3c4f84ecddc3d12ff
hetpatel4381 <hetpatel2808hp@gmail.com>
2024-04-24 08:16:27 +0530

    Implemented a functional graphql api

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-var { graphql, buildSchema } = require("graphql");
+import { graphql, buildSchema } from "graphql";
+import express from "express";
+import { createHandler } from "graphql-http/lib/use/express";
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
@@ -10,19 +12,28 @@ var schema = buildSchema(`
 
 // The rootValue provides a resolver function for each API endpoint
 var rootValue = {
-  hello() {
-    return "Hello world!";
+  hello: () => {
+    return "Hello world 123!";
   },
-  age() {
+  age: () => {
     return 23;
   },
 };
 
 // Run the GraphQL query '{ hello }' and print out the response
-graphql({
-  schema,
-  source: "{ age }",
-  rootValue,
-}).then((response) => {
-  console.log(response);
+// graphql({
+//   schema,
+//   source: "{ age }",
+//   rootValue,
+// }).then((response) => {
+//   console.log(response);
+// });
+
+const app = express();
+
+app.all("/graphql", createHandler({ schema, rootValue }));
+
+app.listen(4000, () => {
+  console.log(`App is Listening to http://localhost:4000`);
+  console.log(`Test: http://localhost:4000/graphql?query={hello,age}`);
 });
